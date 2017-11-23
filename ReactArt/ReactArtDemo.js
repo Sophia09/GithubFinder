d76a3a40798abad1daf43766c27a1f8c09538023
李赛 <lisai@oneapm.com>
2017-11-23 10:35:22 +0800

    using ReactNativeArt in project

---
+++ ReactArt/ReactArtDemo.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import React, { Component } from 'react'
+import {
+    StyleSheet,
+    View,
+} from 'react-native'
+import VectorWidget from './VectorWidget'
+
+export default class ReactArtDemo extends Component {
+    render() {
+        return(
+            <View style={styles.container}>
+                <VectorWidget style={styles.vector}/>
+            </View>
+        );
+    }
+}
+
+var  styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#FFF',
+    },
+    vector: {
+        width: 100,
+        height: 100,
+    },
+
+});
--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 import { AppRegistry } from 'react-native'
 // import GithubFinder from './GithubFinder'
-//import NavDemo from './NavDemo'
-import TabDemo from './pages/TabDemo'
+// import NavDemo from './NavDemo'
+// import TabDemo from './pages/TabDemo'
 //  import DrawerNavDemo from './pages/DrawerNavDemo'
+import ReactArtDemo from './ReactArt/ReactArtDemo'
 
-
-AppRegistry.registerComponent('GithubFinder', () => TabDemo);
+AppRegistry.registerComponent('GithubFinder', () => ReactArtDemo);
