f999e92edf34ea2631977f1905eb6b213f3bac35
李赛 <lisai@oneapm.com>
2017-12-06 15:26:52 +0800

    add function: show sql result in listivew

--- SQLiteManager/index.js
+++ SQLiteManager/index.js
@@ -3,12 +3,4 @@ import { NativeModules } from 'react-native';
 
 const { RNSqLiteManager } = NativeModules;
 
-export default class NativeSqLiteManager {
-    static test() {
-        RNSqLiteManager.test();
-    }
-
-    static initDatabase(callback) {
-        RNSqLiteManager.initDatabase(callback);
-    }
-}
+export default RNSqLiteManager;
--- pages/SQLiteDemo.js
+++ pages/SQLiteDemo.js
@@ -3,19 +3,58 @@ import {
     View,
     Text,
     StyleSheet,
+    ListView,
 } from 'react-native'
 
-import NativeSqLiteManager from '../SQLiteManager/index'
+import RNSqLiteManager from '../SQLiteManager/index'
 
 export default class SQLiteDemo extends Component {
-    render() {
-        // NativeSqLiteManager.test();
-        NativeSqLiteManager.initDatabase((result) => {
-            console.log('first record in Person = ' + result[0]['name'] );
+
+    constructor() {
+        super();
+        this.state = {
+          personArray: new ListView.DataSource({
+              rowHasChanged: (row1, row2) => row1 !== row2,
+          }),
+        };
+    }
+
+    componentDidMount() {
+        RNSqLiteManager.initDatabase((result) => {
+            console.log('name of first person = ' + result[0]['name'] );
+            this.setState({
+                personArray: this.state.personArray.cloneWithRows(result),
+            });
         });
+    }
+
+    render() {
+
         return(
             <View style={styles.container}>
                 <Text style={styles.guide}>SQLite Demo</Text>
+                <View style={styles.topBorder}/>
+                <ListView
+                    dataSource={this.state.personArray}
+                    renderRow={this.showPersonalInfo.bind(this)}
+                    automaticallyAdjustContentInsets={false}
+                    keyboardDismissMode='on-drag'
+                    keyboardShouldPersistTaps='always'
+                    showsVerticalScrollIndicator={true}
+                />
+
+            </View>
+        );
+    }
+
+    showPersonalInfo(person) {
+        return(
+            <View style={styles.personalRow}>
+                <View style={styles.personalInfo}>
+                    <Text>{person.name}</Text>
+                    <Text>{person.address}</Text>
+                </View>
+                <View style={styles.cellBorder}/>
             </View>
         );
     }
@@ -24,10 +63,29 @@ export default class SQLiteDemo extends Component {
 let styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'column',
     },
     guide: {
         padding: 5,
         paddingTop: 40,
+    },
+    personalRow: {
+        padding: 5,
+        flexDirection: 'column',
+    },
+    personalInfo: {
+        flexDirection: 'row',
+        paddingTop: 5,
+    },
+    cellBorder: {
+        backgroundColor: 'black',
+        height: 1,
+        marginTop: 10,
+    },
+    topBorder: {
+        backgroundColor: 'black',
+        height: 1,
+        marginTop: 20,
     },
 });
 
